ae81aff9dd760af2ca65ee4ec334fcb16e67d5ab
chris48s <chris.shaw480@gmail.com>
2017-12-03 12:01:48 +0000

    add tests for get_unique_mailing_lists

--- dc_signup_form/signup_server/tests/test_sync.py
+++ dc_signup_form/signup_server/tests/test_sync.py
@@ -70,3 +70,46 @@ class TestSyncCommand(TestCase):
             record.save()
         users = command.get_new_users(['main_list'])
         self.assertEqual(2, len(list(users)))
+
+    def test_unique_mailing_lists_no_data(self):
+        command = Command()
+        mailing_lists = command.get_unique_mailing_lists()
+        self.assertEqual(0, len(list(mailing_lists)))
+
+    def test_unique_mailing_lists_with_data(self):
+        command = Command()
+        records = [
+            SignupQueue(
+                email='foo@example.com',
+                data={'foo': 'bar'},
+                mailing_lists=['main_list']
+            ),
+            SignupQueue(
+                email='bar@example.com',
+                data={'foo': 'bar'},
+                mailing_lists=['main_list']
+            ),
+            SignupQueue(
+                email='baz@example.com',
+                data={'foo': 'bar'},
+                mailing_lists=['some_other_list', 'main_list']
+            ),
+            SignupQueue(
+                email='qux@example.com',
+                data={'foo': 'bar'},
+                mailing_lists=['main_list', 'some_other_list']
+            ),
+            SignupQueue(
+                email='norf@example.com',
+                data={'foo': 'bar'},
+                mailing_lists=['main_list', 'some_other_list']
+            )
+        ]
+        for record in records:
+            record.save()
+        mailing_lists = command.get_unique_mailing_lists()
+
+        self.assertEqual(3, len(list(mailing_lists)))
+        assert {'mailing_lists': ['main_list']} in list(mailing_lists)
+        assert {'mailing_lists': ['main_list', 'some_other_list']} in list(mailing_lists)
+        assert {'mailing_lists': ['some_other_list', 'main_list']} in list(mailing_lists)
