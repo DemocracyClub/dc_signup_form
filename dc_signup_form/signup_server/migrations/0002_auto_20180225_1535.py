b4ec9542a0a98bbe365e8770804979b4074ccdfa
chris48s <chris.shaw480@gmail.com>
2018-02-25 16:09:13 +0000

    add migration to convert json to jsonb

---
+++ dc_signup_form/signup_server/migrations/0002_auto_20180225_1535.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import django.contrib.postgres.fields.jsonb
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('signup_server', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunSQL("""
+            ALTER TABLE signup_server_signupqueue
+            ALTER COLUMN data
+            SET DATA TYPE jsonb
+            USING data::jsonb;""",
+
+            reverse_sql="""
+            ALTER TABLE signup_server_signupqueue
+            ALTER COLUMN data
+            SET DATA TYPE json
+            USING data::json;"""
+        ),
+
+        migrations.RunSQL("""
+            ALTER TABLE signup_server_signupqueue
+            ALTER COLUMN mailing_lists
+            SET DATA TYPE jsonb
+            USING mailing_lists::jsonb;""",
+
+            reverse_sql="""
+            ALTER TABLE signup_server_signupqueue
+            ALTER COLUMN mailing_lists
+            SET DATA TYPE json
+            USING mailing_lists::json;"""
+        )
+    ]
